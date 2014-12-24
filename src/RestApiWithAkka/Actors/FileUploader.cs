0e06496270f6ca2d658fe9c314b45bcfbf72ed61
kstaruch <kstaruch@gmail.com>
2014-12-24 14:52:02 +0100

    initial

---
+++ src/RestApiWithAkka/Actors/FileUploader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace RestApiWithAkka.Actors
+{
+    public class FileUploadRequest
+    {
+        public string FileName { get; private set; }
+
+        public FileUploadRequest(string fileName)
+        {
+            FileName = fileName;
+        }
+    }
+
+    public class FileUploader: ReceiveActor
+    {
+        public FileUploader()
+        {
+            Receive<FileUploadRequest>(req => Handle(req));
+        }
+
+        protected void Handle(FileUploadRequest req)
+        {
+            Console.WriteLine("FileUploadRequest received: " + req.FileName + " on " + Self.Path + " [" + Self.GetHashCode() + " ]");
+        }
+
+        public static Props Props()
+        {
+            return Akka.Actor.Props.Create(() => new FileUploader());
+        }
+    }
+}
---
+++ src/RestApiWithAkka/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using RestApiWithAkka.Actors;
+
+namespace RestApiWithAkka
+{
+    public class Bootstrapper
+    {
+        public void Run()
+        {
+            var system = ActorSystem.Create("my-system");
+
+            var uploader = system.ActorOf(FileUploader.Props(), "file_uploader");
+
+            var secondUploder = system.ActorOf(FileUploader.Props(), "file_uploader");
+
+            uploader.Tell(new FileUploadRequest("some file name"));
+            secondUploder.Tell(new FileUploadRequest("some file name"));
+        }
+    }
+}
