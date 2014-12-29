06ec8e888b3dc9dd045250f4dedd4b4a4e7b5759
kstaruch <kstaruch@gmail.com>
2014-12-29 01:08:42 +0100

    cloud uploader added

--- src/RestApiWithAkka/Actors/FileUploader.cs
+++ src/RestApiWithAkka/Actors/FileUploader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
+using System.Threading;
 
 namespace RestApiWithAkka.Actors
 {
@@ -19,32 +20,121 @@ namespace RestApiWithAkka.Actors
 
     public class FileUploader: ReceiveActor
     {
-        public FileUploader()
+        // two states initial and uploading for msg indempotence
+
+        private ActorNameNormalizer nameNormalizer;
+
+        public FileUploader(ActorNameNormalizer nameNormalizer)
         {
-            Receive<FileUploadRequest>(req => Handle(req));
+            this.nameNormalizer = nameNormalizer;
+            Receive<FileUploadRequest>(req => HandleInitial(req));
         }
 
-        protected void Handle(FileUploadRequest req)
+        protected void HandleWhenUploading(FileUploadRequest req)
         {
-            Console.WriteLine("FileUploadRequest received: " + req.FileName + " on " + Self.Path + " [" + Self.GetHashCode() + " ]");
+            Console.WriteLine("Ignoring - already uploading " + req.FileName);
+        }
+
+        protected void HandleInitial(FileUploadRequest req)
+        {
+            //Console.WriteLine("FileUploadRequest received: " + req.FileName + " on " + Self.Path + " [" + Self.GetHashCode() + " ]");
+            var newReq = new CloudFileUploadRequest(req.FileName);
+            var child = Context.ActorOf(CloudFileUpload.Props(), "cloud_" + nameNormalizer.NormalizeName(req.FileName));
+            child.Tell(newReq);
+            Become(Uploading);
+        }
+
+        protected void Handle(CloudUploadCompleted req)
+        {
+            Console.WriteLine("CloudUploadCompleted received: " + req.FileName + " on " + Self.Path + " [" + Self.GetHashCode() + " ]");
+            
+            Self.Tell(PoisonPill.Instance); ;
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            Console.WriteLine("PostStop for FileUploader: " + Self.Path + " [" + Self.GetHashCode() + " ]");
+        }
+
+        private void Uploading()
+        {
+            Receive<FileUploadRequest>(req => HandleWhenUploading(req));
+            Receive<CloudUploadCompleted>(req => Handle(req));
+            //TODO: add sth when upload confirmed
         }
 
         public static Props Props()
         {
-            return Akka.Actor.Props.Create(() => new FileUploader());
+            return Akka.Actor.Props.Create(() => new FileUploader(new SimpleActorNameNormalizer()));
+        }
+    }
+
+    public class CloudFileUploadRequest
+    {
+        public string FileName { get; private set; }
+
+        public CloudFileUploadRequest(string fileName)
+        {
+            FileName = fileName;
+        }
+    }
+
+    public class CloudUploadCompleted
+    {
+        public string FileName { get; private set; }
+
+        public CloudUploadCompleted(string fileName)
+        {
+            FileName = fileName;
+        }
+    }
+
+    public class CloudFileUpload: ReceiveActor
+    {
+        private ActorNameNormalizer nameNormalizer;
+
+        public CloudFileUpload(ActorNameNormalizer nameNormalizer)
+        {
+            this.nameNormalizer = nameNormalizer;
+            Receive<CloudFileUploadRequest>(req => Handle(req));
+        }
+
+        protected void Handle(CloudFileUploadRequest req)
+        {
+            Console.WriteLine("CloudFileUploadRequest received: " + req.FileName + " on " + Self.Path + " [" + Self.GetHashCode() + " ]");
+            Thread.Sleep(1000);
+            Console.WriteLine("Upload done");
+            Context.Parent.Tell(new CloudUploadCompleted(req.FileName));
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            Console.WriteLine("PostStop for CloudFileUpload: " + Self.Path + " [" + Self.GetHashCode() + " ]");
+        }
+
+        public static Props Props()
+        {
+            return Akka.Actor.Props.Create(() => new CloudFileUpload(new SimpleActorNameNormalizer()));
         }
     }
 
     public class UploadSupervisor: ReceiveActor
     {
-        public UploadSupervisor()
+        
+        private ActorNameNormalizer nameNormalizer;
+
+        public UploadSupervisor(ActorNameNormalizer nameNormalizer)
         {
+            this.nameNormalizer = nameNormalizer;
             Receive<FileUploadRequest>(req => Handle(req));
+
         }
 
         protected void Handle(FileUploadRequest req)
         {
-            string childName = NormalizeName(req.FileName);
+            string childName = nameNormalizer.NormalizeName(req.FileName);
             var child = Context.Child(childName);
             if (child.IsNobody())
             {
@@ -56,13 +146,22 @@ namespace RestApiWithAkka.Actors
 
         public static Props Props()
         {
-            return Akka.Actor.Props.Create(() => new UploadSupervisor());
+            return Akka.Actor.Props.Create(() => new UploadSupervisor(new SimpleActorNameNormalizer()));
         }
+    }
 
-        private string NormalizeName(string name)
+    public interface ActorNameNormalizer
+    {
+        string NormalizeName(string name);
+    }
+
+    public class SimpleActorNameNormalizer: ActorNameNormalizer
+    {
+        public string NormalizeName(string name)
         {
             return name.Replace(" ", "-");
         }
+
     }
 
 }
